d72d871cf8570dc7872c992609b7daf0914c2214
Abhinav Kumar <38831844+Abhinavk910@users.noreply.github.com>
2020-10-18 20:03:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,11 +2,8 @@
 from flask import Flask
 
 app = Flask(__name__, instance_relative_config=False)
-app.config.from_object('config.Config')
 app.config['SECRET_KEY'] = 'mysecretkey'
 from core.views import core
 app.register_blueprint(core)
-
-
 if __name__ == "__main__":
     app.run(debug = True)
